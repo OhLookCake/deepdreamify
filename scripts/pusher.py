6dd2568628759061381f8c8f91999aba9c325941
Eeshan <eeshan@gmail.com>
2015-07-16 13:57:41 +0530

    posting images to authenticated imgur account

--- scripts/pusher.py
+++ scripts/pusher.py
@@ -1,29 +1,32 @@
 import praw
 import urllib
 import pyimgur
+import pickle
 import logging
 from credentials import *
 
 
 
-def post_to_imgur(imagepath, titletexti, album='WHOXh'):
+def post_to_imgur(imagepath, titletext, descriptiontext, album='WHOXh'):
     imgurposter = logging.getLogger('imgurpost')
     imgurposter.info('Imgur posting initiated')
 
-    CLIENT_ID = IMGUR_CLIENT_ID,
-    CLIENT_SECRET = IMGUR_CLIENT_SECRET
+    #CLIENT_ID = IMGUR_CLIENT_ID
+    #CLIENT_SECRET = IMGUR_CLIENT_SECRET
 
-    im = pyimgur.Imgur(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-    imgurposter.info('Connected to imgur')
-    im.exchange_pin(IMGUR_PIN)
-    imgurposter.info('Pin exchanged')
+    #im = pyimgur.Imgur(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+    #imgurposter.info('Connected to imgur')
+    #im.exchange_pin(IMGUR_PIN)
+    #imgurposter.info('Pin exchanged')
 
-    image = im.upload_image(path=imagepath, title=titletext, description=titletext, album=album)
+    im = pickle.load(open('records/im.ser', 'rb' ))
+    im.refresh_access_token()
+
+    image = im.upload_image(path=imagepath, title=titletext, description=descriptiontext, album=album)
     imgurposter.info('Image uploded')
     imgurposter.info(image.link)
 
-    im.refresh_access_token()
-    print(image.link)
+    return image.link
 
 
 def post(image):
@@ -35,9 +38,7 @@ def post(image):
     postto = 'sometimesitestthings'
 
     fetchlogger = logging.getLogger('fetcher')
-    timestamp = str(int(time.time()))
     fetchlogger.info('Fetcher initiated')
-    fetchlogger.info('Timestamp: ' + timestamp)
 
     # CONFIGURATION
     r = praw.Reddit(user_agent = user_agent)
